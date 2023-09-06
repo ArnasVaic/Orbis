d73a308cd32f214bf1e7d93586c87e79e56db0d3
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2023-09-06 13:59:58 +0300

    gitignore

---
+++ Orbis.UI/Program.cs
@@ -0,0 +1,11 @@
+﻿using OpenTK.Windowing.Desktop;
+using Orbis.UI;
+
+var settings = new NativeWindowSettings
+{
+    Size = (1024, 768),
+    Title = "Title"
+};
+
+var window = new OrbisWindow(GameWindowSettings.Default, settings);
+window.Run();
